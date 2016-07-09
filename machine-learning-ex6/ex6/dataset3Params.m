59e1623332e9c178e1fccd60b75d456400e8ae92
Mark Surnin <marksurnin@gmail.com>
2016-07-09 15:44:27 +0500

    Complete assignment #6.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+% Set the initial error to a very high value.
+min_error = inf;
 
+for C_val = values
+    for sigma_val = values
+        fprintf('Train and evaluate on cross-val set for [C, sigma] = [%f %f]\n', C_val, sigma_val);
+        % Syntax from line 108 in ex6.m
+        model = svmTrain(X, y, C_val, @(x1, x2) gaussianKernel(x1, x2, sigma_val));
+        errs = mean(double(svmPredict(model, Xval) ~= yval));
+        if errs < min_error
+            min_error = errs;
+            C = C_val;
+            sigma = sigma_val;
+            fprintf('[C, sigma] = [%f %f]\n', C, sigma);
+        end
+    end
+end
 
-
-
+fprintf('Best value [C, sigma] = [%f %f] with prediction error = %f.', C, sigma, min_error);
 
 % =========================================================================
 
